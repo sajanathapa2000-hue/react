58b6dea9e7d223f2f62b4e2a32a510ae2e60645f
Sajana Thapa <sajanathapa2000@gmail.com>
2026-09-09 22:47:47 +0545

    update

--- mini-project/src/components/Card.jsx
+++ mini-project/src/components/Card.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+
 import { Bookmark } from 'lucide-react';
-const card = () => {
+const Card = (props) => {
+
+  console.log(props.company)
   return (
          <div className="card">
         <div className="top">
-          <img src="https://thumbs.dreamstime.com/b/icons-sample-psost-setting-428651091.jpg" alt="" />
+          <img src={props.brandLogo} alt="logo" />
           <button>save <Bookmark size={12}/></button>
         </div>
         <div className="center">
-          <h3>Amazon <span>5 days ago</span></h3>
-          <h2>Senior UI/UX Designer</h2>
+          <h3>{props.company} <span>{props.datePosted}</span></h3>
+          <h2>{props.post}</h2>
           <div className='tag'>
-            <h4>Part-Time</h4>
-            <h4>Senior Level</h4>
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
           </div>
         </div>
         <div className="bottom">
           <div>
-              <h3>$120/hr</h3>
-              <p>Mumbai,India</p>
+              <h3>{props.pay}</h3>
+              <p>{props.location}</p>
           </div>
           <div className='eh'>
             <button>Apply Now</button>
@@ -28,4 +30,4 @@ const card = () => {
   )
 }
 
-export default card
+export default Card
