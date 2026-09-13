076a43a355e56cf0803b6246c0840b10f2aa77fc
Sajana Thapa <sajanathapa2000@gmail.com>
2026-09-13 16:23:03 +0545

    added new folder

---
+++ 05-css/src/components/button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+const Button = () => {
+  return (
+    <div>
+      <div className={styles.btn}>
+        this is the button 
+      </div>
+    </div>
+  )
+}
+
+export default Button
---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div>
+      <div className={styles.header}>
+        Header HOO </div>
+    </div>
+  )
+}
+
+export default Header
